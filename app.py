217b294da70542570e1e8087f2332105404c87be
Jaya Krishna Gaddam <115868714+jayakrishnagaddam@users.noreply.github.com>
2024-02-26 06:34:04 +0000

    Added Packager Tools

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template,url_for
-app=Flask(__name__)
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
 @app.route('/')
 def login():
-    return render_template('login.html')
-if __name__=='__main__':
-    app.run(host="0.0.0.0",debug=True)
+  return render_template('login.html')
+
+
+if __name__ == '__main__':
+  app.run(host="0.0.0.0", debug=True)
