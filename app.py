2dab099e5d92adde88cd33eb643cf5ccca5fecd1
Jaya Krishna Gaddam <2100090162@kluniversity.in>
2024-02-26 11:56:37 +0530

    Added Logins

---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template,url_for
+app=Flask(__name__)
+@app.route('/')
+def login():
+    return render_template('login.html')
+if __name__=='__main__':
+    app.run(host="0.0.0.0",debug=True)
